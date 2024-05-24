09f02a48508780cbb6c21bf14ee7975767d51e92
114130854+suikameronn@users.noreply.github.com <114130854+suikameronn@users.noreply.github.com>
2024-05-24 17:49:36 +0900

    モデルを回転させ続ける描画に成功

--- GameManager.cpp
+++ GameManager.cpp
@@ -9,16 +9,10 @@ void GameManager::GameLoop()
 
     VulkanBase::GetInstance()->initVulkan();
     std::unique_ptr<Scene> scene = std::unique_ptr<Scene>(new Scene());
-    /*
-    while (/*!glfwWindowShouldClose(window))
-    {
-    }
-    */
-
-    VulkanBase::GetInstance()->render();
 
     while (!glfwWindowShouldClose(window))
     {
+        VulkanBase::GetInstance()->render();
         glfwPollEvents();
     }
 
