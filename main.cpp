86aae00df63cd64eb7dd2321b003ab336ac1ccd0
sukai3201@au.com <sukai3201@au.com>
2025-03-24 18:46:58 +0900

    no message

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #define _CRTDBG_MAP_ALLOC
 #include <cstdlib>
 #include <crtdbg.h>
@@ -11,11 +13,24 @@ const uint32_t HEIGHT = 600;
 
 GLFWwindow* window;
 
-int main() {
+#ifdef _DEBUG
+int main() 
+{
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);//メモリリーク検出用
     //_CrtSetBreakAlloc(28744);
 
+    glfwInit();//ライブラリの準備
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//ウィンドウの作成
 
+    GameManager* gameManager = GameManager::GetInstance();//GameManager::fpsコントロールやゲームループ
+    gameManager->initGame();//ゲームループの開始
+
+    return EXIT_SUCCESS;
+}
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
     glfwInit();//ライブラリの準備
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//ウィンドウの作成
@@ -26,3 +41,4 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+#endif
