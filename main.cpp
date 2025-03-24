8bb4e5b15c23e167d639e2fe23918e4ba1e908af
sukai3201@au.com <sukai3201@au.com>
2025-03-24 17:03:53 +0900

    no message

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const uint32_t HEIGHT = 600;
 
 GLFWwindow* window;
 
+//#ifdef _DEBUG
 int main() {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);//メモリリーク検出用
     //_CrtSetBreakAlloc(28744);
@@ -26,3 +27,20 @@ int main() {
 
     return EXIT_SUCCESS;
 }
+
+/*
+#else
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
+{
+    glfwInit();//ライブラリの準備
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);//ウィンドウの作成
+
+
+    GameManager* gameManager = GameManager::GetInstance();//GameManager::fpsコントロールやゲームループ
+    gameManager->initGame();//ゲームループの開始
+
+    return EXIT_SUCCESS;
+}
+#endif
+*/
